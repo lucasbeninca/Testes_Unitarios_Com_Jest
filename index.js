31f7caee0882c22c0a36bd96a16f556ff8770402
Lucas <lucasbeninca@proton.me>
2025-05-25 00:24:03 -0300

    implementando-relatorios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function somaHorasExtras(salario, valorHorasExtras) {
     return salario + valorHorasExtras;
 }
 
+
 function calculaDescontos(salario, descontos) {
     return salario - descontos;
 }
